d0b9421fd79a2066f3118c5d2159623e5bc4d851
jgrygoryeva@gmail.com <jgrygoryeva@gmail.com>
2022-12-26 17:58:29 -0800

    Initial commit: Empty project

---
+++ CryptoAsync/CryptoAsync/CryptoAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAsyncApp.swift
+//  CryptoAsync
+//
+//  Created by Yuliya  on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
